f40d4f9d13742ef120f6f34349e2d05c0786cd6f
Anamika Jha <71567445+Anamika1001@users.noreply.github.com>
2022-03-16 22:00:28 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['https://picsum.photos/'],
-  }
+  swcMinify: false
 }
 
 module.exports = nextConfig
